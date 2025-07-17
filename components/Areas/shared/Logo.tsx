eb1ec7aa0eeacdc0f6691bcb6f7fb0a962224940
raviraj879214 <ravirajalphainfo@gmail.com>
2025-07-17 18:02:49 +0530

    t

--- components/Areas/shared/Logo.tsx
+++ components/Areas/shared/Logo.tsx
@@ -4,6 +4,7 @@
 export function Logo({ isFooter = false }) {
   return (
     <div className={`flex items-center space-x-1 ${isFooter ? "text-white" : ""}`}>
+       <img src="/images/Profer logo.png" alt="Logo" />
       <img src="/images/logo.png" alt="Logo" />
     </div>
   );
