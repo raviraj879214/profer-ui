4a337c95f44840b30498f6f5dc0da9e8a592bb68
raviraj879214 <ravirajalphainfo@gmail.com>
2025-07-18 17:38:16 +0530

    logo

--- components/Areas/shared/Logo.tsx
+++ components/Areas/shared/Logo.tsx
@@ -4,15 +4,11 @@
 export function Logo({ isFooter = false }) {
   return (
     <div className={`flex items-center space-x-2 ${isFooter ? "text-white" : ""}`}>
-      <img
-        src="/images/Profer logo.png"
-        alt="Profer Logo"
-        className="h-8 w-auto object-contain"
-      />
+     
       <img
         src="/images/logo.png"
         alt="Secondary Logo"
-        className="h-8 w-auto object-contain"
+        className="h-18 w-auto object-contain"
       />
     </div>
   );
